74ae699b05530feaa801b3542ece30132a4af992
CriticalCow <37348897+CriticalCow@users.noreply.github.com>
2018-10-03 21:27:20 -0700

    Update thanos.py

--- thanos.py
+++ thanos.py
@@ -11,13 +11,16 @@ if choice == 'READY':
   time.sleep(1)                                                                                                     
   choice_2 = raw_input('1  -->  ')                                                                                  
 if choice_2 == '1':                                                                                                 
-  print('wow you can read or count or whatever!')                                                                   
+  print('wow you can read or count or whatever!')
+else: print('can you count')
   choice_3 = raw_input('1 + 1  -->  ')                                                                              
 if choice_3 == '2':                                                                                                 
   print('ok i guess')                                                                                                     
   choice_4 = raw_input('1 * 1  -->  ')                                                                              
 if choice_4 == '1':
-  print('sure')                                                                                                     
+  print('sure')
+else: print('no get it right, 1 times 1 is always 1')
+    time.sleep(1)
   choice_5 = raw_input('10 * 10  -->  ')                                                                            
 if choice_5 == '100':                                                                                               
   print('gud job you can count!')
@@ -25,10 +28,10 @@ if choice_5 == '100':
   time.sleep(1)
   print('nothing! ofc')
   time.sleep(1)
-  choice_6 = raw_input('lets keep going ok?')
+  choice_6 = raw_input('lets keep going ok? ')
 if choice_6 == 'ok':
   time.sleep(1)
-  choice_7 = raw_input('5 * 5')
+  choice_7 = raw_input('5 * 5  -->  ')
 if choice_7 == '25':
   print('ok')
   print('im still making this game wait')                                                                           
